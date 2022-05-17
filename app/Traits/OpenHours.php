dec47ddd222d5eee504f686232c887b5191e2f6e
tony <attila.kovacs92@gmail.com>
2022-05-17 20:16:07 +0200

    add api

--- app/Traits/OpenHours.php
+++ app/Traits/OpenHours.php
@@ -3,12 +3,12 @@ namespace App\Traits;
 
 trait OpenHours
 {
-
     public function getOpenHours()
     {
         $basicHours = config('malom.basicHours');
         $days = collect([ 0 => 'Vasárnap', 1 =>'Hétfő', 2 => 'Kedd', 3 => 'Szerda', 4 => 'Csütörtök', 5 => 'Péntek', 6 => 'Szombat']);
-        $hours = $this->hours->only(['day_of_week', 'open_time', 'close_time']);
+        $hours = $this->hours;
+        // $hours = $this->hours->only(['day_of_week', 'open_time', 'close_time']);
         
         $days->map(function ($day, $key) use ($hours, $basicHours) {
             if (!$hours->contains('day_of_week', $day)) {
