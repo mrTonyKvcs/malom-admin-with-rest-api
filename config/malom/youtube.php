a05662cdb72add0a2a2536d23eb6a6009a7fe81e
kovacs.attila <kovacs.attila@arukereso.com>
2024-01-20 18:20:15 +0100

    new playlist

--- config/malom/youtube.php
+++ config/malom/youtube.php
@@ -5,6 +5,6 @@ return [
     'CHANNEL_ID'    => env('YOUTUBE_CHANNEL_ID', 'UCgb6Gq3ls8Gj-i7zP03gbfg'),
     'playlists'  => [
         'workshop'  => ['PLJkM-F1n7hl1U40TFF5ZubfUOSP1IDAwz'],
-        'vlog'      => ['PLJkM-F1n7hl0A2hqtCRdUcoNI6o1QkdwF', 'PLJkM-F1n7hl3yNP4I9rlEu9SJg2gJDrJn', 'PLJkM-F1n7hl0mnFdaghIdduBu-s7bMScK']
+        'vlog'      => ['PLJkM-F1n7hl1Iz2iUfusIeYVPKfHFjKrm', 'PLJkM-F1n7hl0A2hqtCRdUcoNI6o1QkdwF', 'PLJkM-F1n7hl3yNP4I9rlEu9SJg2gJDrJn', 'PLJkM-F1n7hl0mnFdaghIdduBu-s7bMScK']
     ]
 ];
